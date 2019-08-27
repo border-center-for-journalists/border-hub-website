ffa297a6908302bbbcecb6b44135bd1f6832a462
ErickRc <erick@spaceshiplabs.com>
2019-08-27 12:58:14 -0500

    favicon, pagina generica fix

--- src/components/generalNews/index.styled.js
+++ src/components/generalNews/index.styled.js
@@ -8,42 +8,42 @@ const GeneralNewsSection = styled(Section)`
 `
 
 const GeneralTitle = styled(TitleContainer)`
-    margin: 0 auto;
-h1{
-    color:${props => props.theme.Black}
-}
-h3{
+  margin: 0 auto;
+  h1 {
+    color: ${props => props.theme.Black};
+  }
+  h3 {
     font-size: 25px;
     font-weight: bold;
-}
-p{
-    color:${props => props.theme.Black}
+  }
+  p {
+    color: ${props => props.theme.Black};
     font-size: 19px;
     line-height: 1.58;
-}
-ul{
+  }
+  ul {
     margin: 0 auto 42px;
-}
-ol{
+  }
+  ol {
     margin: 0 auto 42px;
-}
-li{
+  }
+  li {
     font-size: 18px;
-    color:${props => props.theme.Black}
-}
+    color: ${props => props.theme.Black};
+  }
 `
 
 const Quote = styled.p`
-    font-size: 18px;
-    font-style: italic;
-    line-height: 1.33;
-    color:${props => props.theme.Black}
-    border-bottom: 1px solid ${props => props.theme.Black};
-    border-top: 1px solid ${props => props.theme.Black};
-    padding: 23px 50px;
-    b{
-        color:${props => props.theme.Black}
-    }
+  font-size: 18px;
+  font-style: italic;
+  line-height: 1.33;
+  color: ${props => props.theme.Black};
+  border-bottom: 1px solid ${props => props.theme.Black};
+  border-top: 1px solid ${props => props.theme.Black};
+  padding: 23px 50px;
+  b {
+    color: ${props => props.theme.Black};
+  }
 `
 
 export { GeneralNewsSection, GeneralTitle, Quote }
