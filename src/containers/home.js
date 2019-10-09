fb8e95c6d45ed61b70cc237ea6dc4958e9b1fa8f
Sergio Can <gersiopumas@gmail.com>
2019-10-08 22:41:09 -0500

    Added null excerpt error handler Added remove duplicateds function

--- src/containers/home.js
+++ src/containers/home.js
@@ -6,14 +6,43 @@ import RecentNews from "../components/recentNews/index.js"
 import SpecialNews from "../components/specials/index.js"
 
 class HomeContainer extends Component {
+
+  getNoticeUids(notices){
+    return notices.reduce((acc,notice)=>{
+      acc.push(notice.uid);
+      return acc;
+    },[])
+  }
+
+  removeDuplicateds(priorityNotices, normalNotices){
+    let priorityUids = this.getNoticeUids(priorityNotices);
+
+    return normalNotices.reduce((acc,notice)=>{
+      if(!acc.includes(notice) && !priorityUids.includes(notice.uid)){
+        acc.push(notice);
+      }
+      return acc;
+    },[])
+  }
+
   render() {
+    const removeSpecialNotices = this.removeDuplicated(
+      this.props.noticeS.nodes,
+      this.props.recentNotices.nodes
+    );
+    const recentNoticesFiltered = {
+      nodes: this.removeDuplicated(
+        this.props.noticeP.nodes,
+        removeSpecialNotices
+      )
+    }
     return (
       <React.Fragment>
         <HomeHeaderComponent bannerNotice={this.props.bannerNotice} />
         <MainNewsComponent notice={this.props.noticeP} />
         <SubscribeComponent />
         <RecentNews
-          notices={this.props.recentNotices}
+          notices={recentNoticesFiltered}
           bannerNotices={this.props.bannerNotice}
           principalNotices={this.props.normalNotices}
         />
