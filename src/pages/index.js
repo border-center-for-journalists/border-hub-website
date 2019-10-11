18128fdb2328c8cdaaee3bd2b77ea4817f9c7ccd
Sergio Can <gersiopumas@gmail.com>
2019-10-11 15:18:52 -0500

    Added instagram to authors query

--- src/pages/index.js
+++ src/pages/index.js
@@ -291,6 +291,9 @@ export const pageQuery = graphql`
             twitter {
               text
             }
+            instagram {
+              text
+            }
           }
           alliances {
             alliance_image {
