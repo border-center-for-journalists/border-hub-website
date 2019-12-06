e5eb70398554b3b395f1884871fbb440445d7d18
Pablo Melendez <pablofer.melendez@gmail.com>
2019-12-06 11:30:55 -0500

    add simple validation

--- src/components/notice/alliances.js
+++ src/components/notice/alliances.js
@@ -8,7 +8,7 @@ const AlliancesNoticeContentComponent = ({ alliances }) => {
       <NoticeSectionTitle>Alianzas</NoticeSectionTitle>
       <Rows wrap align="center" vAlign="center" gap="10px">
         {alliances.map((alliance, index) =>
-          alliance.alliance_image.url ? (
+          alliance.alliance_image && alliance.alliance_image.url ? (
             <a href={alliance.alliance_image.url} key={index}>
               <img alt="" src={alliance.alliance_image.url} />
             </a>
