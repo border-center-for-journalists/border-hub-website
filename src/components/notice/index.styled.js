ee1947548b98dcaf8c8046b854a94cd0b7282fd4
Pablo Melendez <pablofer.melendez@gmail.com>
2020-01-24 13:21:22 -0500

    fix typo

--- src/components/notice/index.styled.js
+++ src/components/notice/index.styled.js
@@ -10,7 +10,7 @@ const NoticeSection = styled.section`
 `
 
 const NormalNoticeContainer = styled.div`
-  widht: 100%;
+  width: 100%;
   height: 100%;
   background-color: ${props => props.theme.White};
 `
