8dc4a3910fbf8bd9a93b82c4b4fbe65465b04d75
Hector Can <habacab@gmail.com>
2023-05-18 10:18:17 -0500

    Fixed styles btn was wrong some points where untouchable

--- src/components/notice/donate.js
+++ src/components/notice/donate.js
@@ -10,7 +10,17 @@ const NormalDonateComponent = () => {
         <i className="icon-donar" />
         <h2>El periodismo requiere de tu apoyo</h2>
         <p>Conviértete en miembro del Border Hub</p>
-        <button><a href="https://www.buymeacoffee.com/borderhub" target="_blank" rel="noopener noreferrer" style={{ color: 'inherit', textDecoration: 'none' }}>Donar</a></button>
+          <a
+            href="https://www.buymeacoffee.com/borderhub"
+            target="_blank"
+            className="bmc-button"
+            rel="noopener noreferrer"
+            style={{
+              margin: "1.23em auto 0"
+            }}
+          >
+            Donar
+          </a>
       </DonateContainer>
     </React.Fragment>
   )
