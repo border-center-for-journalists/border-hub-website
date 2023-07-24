aa83d763c3e76d9969e3eaf81ad4e5a37fe1ef79
JFrank510 <150300163@ucaribe.edu.mx>
2023-07-23 21:37:43 -0500

    Only Authors

--- src/utils/generic.js
+++ src/utils/generic.js
@@ -7,9 +7,24 @@ const getDate = (date) => {
 }
 
 const getAuthor = (author) => {
+  const excludedTitles = ["Ilustración", "Verificado", "Edición"];
+
   const authorNames = author
-      .filter(author => author.name && author.name.text !== "")
-      .map(author => author.name.text)
+    .filter(author => {
+      if (!author.name || !author.name.text || author.name.text.trim() === "") {
+        return false;
+      }
+
+      // Descomponemos el nombre del autor en partes basándonos en "|"
+      const authorParts = author.name.text.split("|").map(part => part.trim());
+
+      // Solo incluimos autores que no tienen un título excluido en su nombre
+      return authorParts.length < 2 || !excludedTitles.includes(authorParts[1]);
+    })
+    .map(author => {
+      const parts = author.name.text.split("|");
+      return parts.length > 1 ? parts[0].trim() : author.name.text.trim();
+    });
 
   if (authorNames.length > 0) {
     const names =  authorNames.join(', ');
